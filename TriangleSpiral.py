59c1fbddd6b256619fc5259e7570648bf2edaec1
SnehaShree2501 <snehashree2501@gmail.com>
2020-12-08 21:52:00 +0530

    My first commit

---
+++ TriangleSpiral.py
@@ -0,0 +1,14 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+candy = turtle.Turtle()
+candy.color("red")
+candy.shape("turtle")
+dist = 5
+candy.up()
+for x in range(45):
+    candy.stamp()
+    candy.forward(dist)
+    candy.right(24)
+    dist = dist +2
+wn.exitonclick()  
